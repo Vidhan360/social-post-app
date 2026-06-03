74efc02bf0d345549e2b4bf7c24e633431ab82ef
Vidhan360 <mishravidhan110@gmail.com>
2026-06-03 15:47:38 +0530

    configure production api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,16 +2,13 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    import.meta.env
-      .VITE_API_URL,
+    import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use(
   (config) => {
     const token =
-      localStorage.getItem(
-        "token"
-      );
+      localStorage.getItem("token");
 
     if (token) {
       config.headers.Authorization =
